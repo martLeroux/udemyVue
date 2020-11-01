30cc61cc98bfda8a198d584d811abcda81ac2318
Leroux Martin <tintin_151@hotmail.com>
2020-10-31 20:53:26 -0400

    #16

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,8 +1,19 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: 'Finish the course and learn Vue!'
+      courseGoal: 'Finish the course and learn Vue!',
+      vueLink: 'https://vuejs.org'
     };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return 'Learn Vue!';
+      } else {
+        return 'Master Vue!';
+      }
+    }
   }
 });
 
