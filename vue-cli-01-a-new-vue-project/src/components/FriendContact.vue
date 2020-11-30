7090d7a60e466d704c0c7e1623c4c7d02326daaf
Leroux Martin <tintin_151@hotmail.com>
2020-11-29 20:34:12 -0500

    #91: Validation props

--- vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <header><h1>{{ name }} {{ friendIsFavorite === "1" ? '(Favorite)' : ''}}</h1></header>
+    <header><h1>{{ name }} {{ isFavorite === "1" ? '(Favorite)' : ''}}</h1></header>
     <button @click="toggleFavorite">Toggle Favorite</button>
     <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
     <ul v-if="detailsAreVisible">
@@ -12,7 +12,29 @@
 
 <script>
 export default {
-  props: ["name", "phoneNumber", "emailAddress", "isFavorite"],
+  //props: ["name", "phoneNumber", "emailAddress", "isFavorite"],
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    phoneNumber: {
+      type: String,
+      required: true
+    },
+    emailAddress: {
+      type: String,
+      required: true
+    },
+    isFavorite: {
+      type: String,
+      required: false,
+      default: "0",
+      validator: function(value) {
+        return value === "1" || value === "0";
+      }
+    }
+  },
   data() {
     return {
       detailsAreVisible: false,
