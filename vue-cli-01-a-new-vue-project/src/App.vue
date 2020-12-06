b32fb7bca44b1c902b337e4699c0d075225632b0
Leroux Martin <tintin_151@hotmail.com>
2020-12-06 10:30:55 -0500

    #97: add new friend

--- vue-cli-01-a-new-vue-project/src/App.vue
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -1,6 +1,7 @@
 <template>
 <section>
   <h2>My friends</h2>
+  <new-friend @add-contact="addContact"></new-friend>
   <ul>
     <friend-contact v-for="friend in friends" :key="friend.id" :id="friend.id" :name="friend.name" :phone-number="friend.phone" :email-address="friend.email" v-bind:is-favorite="friend.isFavorite" @toggle-favorite="toggleFavoriteStatus"></friend-contact>
   </ul>
@@ -35,6 +36,16 @@ export default {
         friend => friend.id === friendId
       );
       identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+    },
+    addContact(name, phone, email) {
+      const newFriendContact = {
+        id: new Date().toISOString(),
+        name: name,
+        phone: phone,
+        email: email,
+        isFavorite: false
+      };
+      this.friends.push(newFriendContact);
     }
   }
 };
@@ -71,7 +82,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -103,5 +115,21 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+
+#app form div {
+  margin: 1rem 0;
 }
 </style>
