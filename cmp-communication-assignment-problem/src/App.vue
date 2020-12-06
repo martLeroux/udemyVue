dee454bb93eb97da628a1cfee0070ded48475a39
Leroux Martin <tintin_151@hotmail.com>
2020-12-06 11:25:10 -0500

    98 assignment

---
+++ cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<section>
+  <active-user v-for="user in users" :key="user.id" :id="user.id" :username="user.username" :age="user.age" ></active-user>
+  <user-data @update-username="updateUsername"></user-data>
+
+</section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: [
+        {
+          id: "tintin_151",
+          username: "tintin_151",
+          age: 35
+        }
+      ]
+    };
+  },
+  methods: {
+    updateUsername(username, age) {
+      this.users[0].username = username;
+      this.users[0].age = +age;
+    }
+  }
+};
+</script>
---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,32 @@
+<template>
+<form @submit.prevent="submitData">
+  <div>
+    <label for="name">Name</label>
+    <input type="text" id="name" name="name" v-model="enteredUsername"/>
+  </div>
+  <div>
+    <label for="age">Age</label>
+    <input type="text" id="age" name="age" v-model="enteredAge"/>
+  </div>
+  <div>
+    <button>Update username</button>
+  </div>
+</form>
+</template>
+
+<script>
+export default {
+  emits: ["update-username"],
+  data() {
+    return {
+      enteredUsername: "",
+      enteredAge: null
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit("update-username", this.enteredUsername, this.enteredAge);
+    }
+  }
+};
+</script>
