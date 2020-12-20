1961c5e2132bc5822e4826cace5d722313990a6c
Leroux Martin <tintin_151@hotmail.com>
2020-12-20 14:57:39 -0500

    #126

---
+++ prj-cmp-01-starting-setup/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <learning-resource v-for="resource in resources" v-bind:key="resource.id"
+      :title="resource.title" :description="resource.description" :link="resource.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource';
+
+export default {
+  components: {
+    LearningResource
+  },
+  props: ['resources']
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
