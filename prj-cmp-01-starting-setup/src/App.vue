f9f9419069bc4522157f1affb6e15c024095e6de
Leroux Martin <tintin_151@hotmail.com>
2020-12-20 14:49:07 -0500

    #125

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul>
+    <learning-resource v-for="resource in this.storedResources" v-bind:key="resource.id"
+      :title="resource.title" :description="resource.description" :link="resource.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource';
+
+export default {
+  components: {
+    LearningResource
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google',
+          link: 'https:www.google.com'
+        }
+      ]
+    };
+  }
+};
+</script>
