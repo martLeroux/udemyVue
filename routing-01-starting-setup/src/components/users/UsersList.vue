fc897d747cb9fc20342c847842b1937a704403f4
Leroux Martin <tintin_151@hotmail.com>
2021-01-03 20:15:18 -0500

    #183

--- routing-01-starting-setup/src/components/users/UsersList.vue
+++ routing-01-starting-setup/src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
   <button @click="confirmInput">Confirm</button>
+  <button @click="saveChanges">Save changes</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -12,17 +13,38 @@ export default {
   components: {
     UserItem
   },
+  data() {
+    return {
+      changesSaved: false
+    };
+  },
   inject: ['users'],
   methods: {
     confirmInput() {
       // do something
       this.$router.push('/teams');
+    },
+    saveChanges() {
+      this.changesSaved = true;
     }
   },
   beforeRouteEnter(to, from, next) {
     console.log('UsersList Cmp beforeRouteEnter');
     console.log(to, from);
     next();
+  },
+  beforeRouteLeave(to, from, next) {
+    console.log('UsersList Cmp beforeRouteLeave');
+    console.log(to, from);
+    if (this.changesSaved) {
+      next();
+    } else {
+      const userWantsToLeave = confirm('Are you sure? You got unsaved data');
+      next(userWantsToLeave);
+    }
+  },
+  unmounted() {
+    console.log('UsersList unmounted');
   }
 };
 </script>
