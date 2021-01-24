707f1b38ee37928245ec653a61cf449949630b8d
Leroux Martin <tintin_151@hotmail.com>
2021-01-24 10:34:00 -0500

    #216

--- vuex-01-starting-setup/src/components/ChangeCounter.vue
+++ vuex-01-starting-setup/src/components/ChangeCounter.vue
@@ -1,13 +1,21 @@
 <template>
-  <button @click="addTwo">Add 2 * 2</button>
+  <button @click="increase({value: 2})">Increase 2 * 2</button>
+  <button @click="increment">Increment 1 * 2</button>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   methods: {
-    addTwo() {
+    /*addTwo() {
       this.$store.dispatch('increase', { value: 2 });
-    }
+    }*/
+    ...mapActions(['increase', 'increment'])
+    /*...mapActions({
+      inc: 'increment',
+      increase: 'increase'
+    })*/
   }
 };
 </script>
--- vuex-01-starting-setup/src/components/TheCounter.vue
+++ vuex-01-starting-setup/src/components/TheCounter.vue
@@ -1,13 +1,16 @@
 <template>
-  <h3>{{ counter }}</h3>
+  <h3>{{ finalCounter }}</h3>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
-  computed: {
+  /*computed: {
     counter() {
       return this.$store.getters.finalCounter;
     }
-  }
+  }*/
+  ...mapGetters(['finalCounter'])
 };
 </script>
