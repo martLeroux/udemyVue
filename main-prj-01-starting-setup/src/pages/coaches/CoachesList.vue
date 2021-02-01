12c0dc0aed24bcfec24ba375cabb9cd01dcd72d0
Leroux Martin <tintin_151@hotmail.com>
2021-01-31 19:27:51 -0500

    #237

--- main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -3,6 +3,31 @@
     Filter
   </section>
   <section>
-    LIST of COACHES
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
+    <ul v-if="hasCoaches">
+      <coach-item v-for="coach in filteredCoaches" :key="coach.id" :first-name="coach.firstName" :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
+    </ul>
+    <h3 v-else>No coaches found.</h3>
   </section>
 </template>
+
+<script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+
+export default {
+  components: { CoachItem },
+  computed: {
+    filteredCoaches() {
+      //return this.$store.getters.coaches;
+      // à cause du namespace
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    }
+  }
+};
+</script>
