1523c6227993f43a54d09f3596fb289ac94d715d
Leroux Martin <tintin_151@hotmail.com>
2021-02-14 17:06:33 -0500

    #252

--- main-prj-01-starting-setup/src/store/modules/requests/actions.js
+++ main-prj-01-starting-setup/src/store/modules/requests/actions.js
@@ -1,11 +1,54 @@
 export default {
-  contactCoach(context, payload) {
+  async contactCoach(context, payload) {
     const newRequest = {
-      id: new Date().toISOString(),
-      coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message
     }
+
+
+
+    const response = await fetch(`https://vue-main-project-57792-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`, {
+      method: 'POST',
+      body: JSON.stringify(newRequest)
+    });
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to send');
+      throw error
+    }
+
+    newRequest.id = responseData.name;
+    newRequest.coachId = payload.coachId;
+
     context.commit('addRequest', newRequest);
+  },
+  async fetchRequests(context) {
+    const coachId = context.rootGetters.userId;
+
+    console.log('yoooo');
+    const response = await fetch(`https://vue-main-project-57792-default-rtdb.firebaseio.com/requests/${coachId}.json`);
+    console.log(response);
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to fetch requests');
+      throw error
+    }
+
+    //parce qu'on recoit un objet et qu'on a besoin de mettre ça dans un array
+    const requests = [];
+    for (const key in responseData) {
+      const request = {
+        id: key,
+        coachId: coachId,
+        userEmail: responseData[key].userEmail,
+        message: responseData[key].message
+      }
+      requests.push(request);
+    }
+
+    context.commit('setRequests', requests);
   }
 }
