9051548543934383ecdf107e3fd6cebca3f79985
Leroux Martin <tintin_151@hotmail.com>
2021-02-14 10:03:26 -0500

    #247

--- main-prj-01-starting-setup/src/store/modules/requests/actions.js
+++ main-prj-01-starting-setup/src/store/modules/requests/actions.js
@@ -2,6 +2,7 @@ export default {
   contactCoach(context, payload) {
     const newRequest = {
       id: new Date().toISOString(),
+      coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message
     }
