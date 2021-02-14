bcceaab05fd6043ac97427633c47856566b31463
Leroux Martin <tintin_151@hotmail.com>
2021-02-14 11:23:54 -0500

    #250

--- main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -6,9 +6,12 @@
     <base-card>
       <div class="controls">
         <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
-        <base-button link to="/register" v-if="!isCoach">Register as Coach</base-button>
+        <base-button link to="/register" v-if="!isCoach && !isLoading">Register as Coach</base-button>
       </div>
-      <ul v-if="hasCoaches">
+      <div v-if="isLoading">
+        <base-spinner></base-spinner>
+      </div>
+      <ul v-else-if="hasCoaches">
         <coach-item v-for="coach in filteredCoaches" :key="coach.id" :first-name="coach.firstName" :id="coach.id" :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
       </ul>
       <h3 v-else>No coaches found.</h3>
@@ -24,6 +27,7 @@ export default {
   components: { CoachItem, CoachFilter },
   data() {
     return {
+      isLoading: false,
       activeFilters: {
         frontend: true,
         backend: true,
@@ -51,7 +55,7 @@ export default {
       });
     },
     hasCoaches() {
-      return this.$store.getters['coaches/hasCoaches'];
+      return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
     },
     isCoach() {
       return this.$store.getters['coaches/isCoach'];
@@ -64,8 +68,10 @@ export default {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
     },
-    loadCoaches() {
-      this.$store.dispatch('coaches/loadCoaches');
+    async loadCoaches() {
+      this.isLoading = true;
+      await this.$store.dispatch('coaches/loadCoaches');
+      this.isLoading = false;
     }
   }
 };
