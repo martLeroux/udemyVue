26ebc2aa8d57a6b28cbc35abd549f7e58422060d
Leroux Martin <tintin_151@hotmail.com>
2021-03-01 13:48:54 -0500

    #292

--- composition-01-starting-setup/src/components/UserData.vue
+++ composition-01-starting-setup/src/components/UserData.vue
@@ -10,10 +10,12 @@ import { computed } from 'vue';
 
 export default {
   props: ['firstName', 'lastName', 'age'],
-  setup(props) {
+  setup(props, context) {
     const userName = computed(function() {
       return props.firstName + ' ' + props.lastName;
     });
+
+    context.emit('save-data', 1); //this.$emit('save-data', 1);
 
     return { userName };
   }
